69ba449f6957bb10fc5ef873b4704b34afe06685
sydney <snthomas@hotmail.ca>
2019-12-23 19:36:41 -0500

    Finished initial Game class, Player class, PlayerStats class, Season class and Team class. Added classes for StatsManager and UndoStack

--- app/src/main/java/Season.java
+++ app/src/main/java/Season.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Season {
     private ArrayList<Game> games;
+    private Team roster;
+    private int startYear;
+    private int endYear;
     public Season(){
-
+        startYear = Calendar.getInstance().get(Calendar.YEAR);
+        endYear = startYear+1;
+        ArrayList<Game> games = new ArrayList<Game>();
     }
-    public Season(Team t, int year){
+    public Season(Team t, int sYear, int eYear){
+        roster = t;
+        startYear = sYear;
+        endYear = eYear;
 
     }
 
@@ -15,11 +25,43 @@ public class Season {
     public void setGames(ArrayList<Game> g) {
         games = g;
     }
-    /*
-    public double getPointsPerGame(){
 
+    public Team getRoster() {
+        return roster;
     }
-    public String toString(){
 
-    }*/
+    public void setRoster(Team r) {
+        roster = r;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(int sYear) {
+        startYear = sYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(int eYear) {
+        endYear = eYear;
+    }
+    public Game getGame(long dateTime){
+        for(Game g: this.games) {
+            if(g.getGameDateTime() == dateTime){
+                return g;
+            }
+        }
+        return null;
+    }
+
+    public String toString() {
+        String str = "";
+        str += "Start Year: " + startYear + "\nEnd Year: " + endYear;
+        //TODO add missing field
+        return str;
+    }
 }
