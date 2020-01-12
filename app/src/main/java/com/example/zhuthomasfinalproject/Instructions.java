8947cc0b62ec82b934c93d42154126cf30655ee7
jezhu8928 <jessica.jz.zhu@gmail.com>
2020-01-12 13:57:41 -0500

    added file reading capabilities to instructions

--- app/src/main/java/com/example/zhuthomasfinalproject/Instructions.java
+++ app/src/main/java/com/example/zhuthomasfinalproject/Instructions.java
@@ -1,13 +1,34 @@
 package com.example.zhuthomasfinalproject;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Instructions extends AppCompatActivity {
+    private TextView instructions;
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instructions);
         //yourTextView.setMovementMethod(new ScrollingMovementMethod());
+        instructions = findViewById(R.id.instruct_text);
+
+        String text = "";
+
+        try {
+            InputStream is = getAssets().open("instructions.txt");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            text = new String(buffer);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        instructions.setText(text);
     }
 }
