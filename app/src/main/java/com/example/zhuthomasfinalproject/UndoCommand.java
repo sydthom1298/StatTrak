f29bfe9a89f09f85a7eb4e2d63cb74ef6064a89c
sydney <snthomas@hotmail.ca>
2020-01-14 18:06:31 -0500

    Commented UndoCommand class

--- app/src/main/java/com/example/zhuthomasfinalproject/UndoCommand.java
+++ app/src/main/java/com/example/zhuthomasfinalproject/UndoCommand.java
@@ -1,8 +1,11 @@
+/**
+ * Sydney Thomas
+ * January 14, 2020
+ * Class that is the commands to save on the undo stack
+ */
 package com.example.zhuthomasfinalproject;
-
 public class UndoCommand {
-
-
+    //indicators to tell what stat was just updated so it can be undone
     public static final int MAKE_3PT = 1;
     public static final int MAKE_2PT = 2;
     public static final int MAKE_FT = 3;
@@ -17,27 +20,55 @@ public class UndoCommand {
     public static final int BLK = 12;
     public static final int FOUL = 13;
 
-    private int cmd;
-    private Player player;
-    
+    private int cmd; //one of the commands above
+    private Player player; //player the stat was entered for
+
+    /**
+     * default constructor that constructs an UndoCommand class
+     */
+    public UndoCommand(){
+
+    }
+
+    /**
+     * constructor that contructs a UndoCommand class with parameters and initializes attributes
+     * @param c - command to undo
+     * @param p - player the command was for
+     */
     public UndoCommand(int c, Player p) {
         cmd = c;
         player = p;
     }
 
+    /**
+     * accessor that gets the command to undo
+     * @return - command to undo
+     */
     public int getCmd() {
         return cmd;
     }
 
-    public void setCmd(int cmd) {
-        this.cmd = cmd;
+    /**
+     * mutator that sets the command to undo
+     * @param c - command to undo
+     */
+    public void setCmd(int c) {
+        cmd = c;
     }
 
+    /**
+     * accessor that gets the player the command to undo was for
+     * @return - player command was for
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * mutator that sets the player the command undo was for
+     * @param p - player command was for
+     */
     public void setPlayer(Player p) {
-        this.player = player;
+        player = p;
     }
 }
