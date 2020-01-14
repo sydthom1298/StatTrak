1cc36aef62f322ca23a4ae0a1d8617dbf035bde7
sydney <snthomas@hotmail.ca>
2020-01-14 18:21:18 -0500

    Commented Team class

--- app/src/main/java/com/example/zhuthomasfinalproject/Team.java
+++ app/src/main/java/com/example/zhuthomasfinalproject/Team.java
@@ -1,19 +1,15 @@
 /**
  * Sydney Thomas
  * January 14, 2020
- *
+ * Class that encapsulates a team
  */
 package com.example.zhuthomasfinalproject;
-
-import com.example.zhuthomasfinalproject.Player;
-import com.example.zhuthomasfinalproject.Season;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 public class Team implements Serializable {
-    private String name;
-    private ArrayList<Player> players;
-    private ArrayList<Season> seasons;
+    private String name; //name of team
+    private ArrayList<Player> players; //ArrayList of players
+    private ArrayList<Season> seasons; //ArrayList of seasons
 
     /**
      * default constructor that constructs a team class and initializes attributes
@@ -60,65 +56,99 @@ public class Team implements Serializable {
         name = n;
     }
 
-
+    /**
+     * accessor that gets the number of players on team
+     * @return - number of players
+     */
     public int getNumPlayers() {
         return players.size();
     }
 
 
     /**
-     * accessor to return players
-     * @return player ArrayList
+     * accessor that gets players list
+     * @return - players ArrayList
      */
     public ArrayList<Player> getPlayers() {
         return players;
     }
 
+    /**
+     * mutator that sets players list
+     * @param p - players ArrayList
+     */
     public void setPlayers(ArrayList<Player> p) {
         players = p;
     }
-    /*
-    TODO LATER
-    public com.example.zhuthomasfinalproject.Team clone(){
 
-    }*/
-
-
-
+    /**
+     * method that finds the player with the name and number
+     * @param name - name searching for
+     * @param num - number searching for
+     * @return - found player or null if not found
+     */
     public Player findPlayer(String name, int num) {
-        for(Player p : this.players){
+        //loop through players ArrayList
+        for(Player p : players){
+            //finds player
             if(p.getName().equals(name) && (num == p.getJerseyNum())){
                 return p;
             }
         }
+        //player wasn't found
         return null;
     }
 
+    /**
+     * method that adds a player to a team
+     * @param p - player to add
+     */
     public void addPlayer(Player p) {
         players.add(p);
     }
 
+    /**
+     * method that removes a player from a team
+     * @param p - player to remove
+     */
     public void removePlayer(Player p){
         players.remove(p);
     }
 
+    /**
+     * accessor that gets the season ArrayList
+     * @return - season ArrayList
+     */
     public ArrayList<Season> getSeasons() {
         return seasons;
     }
 
+    /**
+     * accessor that gets the season for the specified start year
+     * @param startYear - start year
+     * @return - season found, null if not found
+     */
     public Season getSeason(int startYear) {
-        for (int i = 0; i < seasons.size(); i++) {
-            if (seasons.get(i).getStartYear() == startYear) {
+        for(int i = 0; i < seasons.size(); i++) {
+            if(seasons.get(i).getStartYear() == startYear) {
                 return seasons.get(i);
             }
         }
         return null;
     }
 
+    /**
+     * mutator that sets season ArrayList
+     * @param s - season ArrayList
+     */
     public void setSeasons(ArrayList<Season> s) {
         seasons = s;
     }
 
+    /**
+     * method that adds a season
+     * @param s - season to add
+     */
     public void addSeason(Season s) {
         seasons.add(s);
     }
@@ -128,9 +158,7 @@ public class Team implements Serializable {
      * @return - String representation
      */
     public String toString(){
-
         String str = "";
-        //str += "Name: " + name+ "\nPlayers: " + players;
         str += name;
         //TODO add missing fields
         return str;
