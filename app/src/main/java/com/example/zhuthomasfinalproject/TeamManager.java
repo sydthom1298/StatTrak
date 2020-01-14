decd6cdc17c7fc683b461464957d16dfa5c0c6aa
sydney <snthomas@hotmail.ca>
2020-01-14 17:59:51 -0500

    Commented TeamManager

--- app/src/main/java/com/example/zhuthomasfinalproject/TeamManager.java
+++ app/src/main/java/com/example/zhuthomasfinalproject/TeamManager.java
@@ -107,95 +107,152 @@ public class TeamManager extends AppCompatActivity implements AdapterView.OnItem
 
     /**
      * user has selected an item from the spinner
-     * @param parent - table
+     * @param parent - spinner
      * @param view - comes with event
      * @param pos - row of the selected item in table
      * @param id - comes with event
      */
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-        Team t;
+        Team t; //team selected from spinner control
+        EditText n; //name control
+        EditText num; //number control
+        EditText position; //position control
+        CheckBox chk; //active checkbox
+        TableRow r; //table row
+        Player p;
+
+        //ignore the first selection event
         if(firstTime){
             firstTime = false;
             return;
         }
+        //remove any players from player table
         clearTable();
+        //get the currently selected team from the spinner
         t = (Team) parent.getItemAtPosition(pos);
+        //if the user selects the prompt entry for team instead of a real team
         if(t.getName().equals("Select team to edit")){
             playersCount = 0;
             txt_numPlayers.setText(Integer.toString(playersCount));
             enableButtons(false);
-        }else{
+        }else{ //its a real team
             enableButtons(true);
         }
+        //saves team as selected team
         selectedTeam = t;
+        //add all the player into the player table
         for(int i = 0; i < t.getNumPlayers(); i++){
-            TableRow r = new TableRow(this);
-            EditText n,num,position;
-            CheckBox chk;
-            r.addView(n =newName());
-            r.addView(num = newNumber());
-            r.addView(position = newPos());
-            r.addView(chk = newCheck());
-            Player p = t.getPlayers().get(i);
+            //create a new table row
+            r = new TableRow(this);
+            //create controls for within table row
+            n = newName();
+            num = newNumber();
+            position = newPos();
+            chk = newCheck();
+            //add controls to table row
+            r.addView(n);
+            r.addView(num);
+            r.addView(position);
+            r.addView(chk);
+            //get player from team object
+            p = t.getPlayers().get(i);
+            //set data from player to controls for table row
             n.setText(p.getName());
             num.setText(Integer.toString(p.getJerseyNum()));
             position.setText(p.getPosition());
             chk.setEnabled(p.isActive());
-
+            //add table row to table
             tbl_players.addView(r,tbl_row.getLayoutParams());
+            //updates player count
             playersCount++;
             txt_numPlayers.setText(Integer.toString(playersCount));
         }
 
     }
+
+    /**
+     * method required for the interface for when nothing is selected
+     * @param parent - window
+     */
     public void onNothingSelected(AdapterView<?> parent){
-        String a = "Test";
+
     }
     /**
      * method to create new team when the create new team button is pressed
      * @param v - current window
      */
     public void onCreateNewTeam(View v){
-        String teamName;
+        String teamName; //name of team
+        //get name of team from edit text control
         teamName = txt_newTeam.getText().toString();
+        //create a new team
         team = new Team(teamName);
         StatsManager.addTeam(team);
+        //clears table, set number of players to zero
         clearTable();
         txt_numPlayers.setText(Integer.toString(0));
         playersCount = 0;
-        TextView t = (TextView)spn_team.getChildAt(spn_team.getChildCount()-1);
         txt_newTeam.setText("");
-        spn_team.setSelection(spn_team.getAdapter().getCount()-1);
-        spn_team.setSelection(spn_team.getAdapter().getCount()-1);
+        //set spinner to new team added
+        spn_team.setSelection(spn_team.getAdapter().getCount() - 1);
 
     }
+
+    /**
+     * method that generates a new edit text control for the name
+     * @return - new edit text control
+     */
     EditText newName() {
-        EditText e = new EditText(this);
-        EditText orig = (EditText)(tbl_row.getVirtualChildAt(0));
+        EditText e;
+        EditText orig;
+        e = new EditText(this);
+        //copies layout info from orig to new e control
+        orig = (EditText)(tbl_row.getVirtualChildAt(0));
         e.setLayoutParams(tbl_row.getVirtualChildAt(0).getLayoutParams());
         e.setFontFeatureSettings(orig.getFontFeatureSettings());
         e.setTypeface(orig.getTypeface());
         e.setTextSize(orig.getTextSize());
         return e;
     }
+
+    /**
+     * method that generates a new edit text control for number
+     * @return - new edit text control
+     */
     EditText newNumber() {
-        EditText e = new EditText(this);
-        EditText orig = (EditText)(tbl_row.getVirtualChildAt(1));
+        EditText e;
+        EditText orig;
+        e = new EditText(this);
+        //copies layout info from orig to new e control
+        orig = (EditText)(tbl_row.getVirtualChildAt(1));
         e.setFontFeatureSettings(orig.getFontFeatureSettings());
         e.setTypeface(orig.getTypeface());
         e.setTextSize(orig.getTextSize());
         return e;
     }
+    /**
+     * method that generates a new edit text control for position
+     * @return - new edit text control
+     */
     EditText newPos() {
-        EditText e = new EditText(this);
-        EditText orig = (EditText)(tbl_row.getVirtualChildAt(2));
+        EditText e;
+        EditText orig;
+        e = new EditText(this);
+        //copies layout info from orig to new e control
+        orig = (EditText)(tbl_row.getVirtualChildAt(2));
         e.setFontFeatureSettings(orig.getFontFeatureSettings());
         e.setTypeface(orig.getTypeface());
         e.setTextSize(orig.getTextSize());
         return e;
     }
+
+    /**
+     * method that generates a new active player check box
+     * @return - new check box control
+     */
     CheckBox newCheck() {
-        CheckBox c = new CheckBox(this);
+        CheckBox c;
+        c = new CheckBox(this);
         c.setLayoutParams(tbl_row.getVirtualChildAt(3).getLayoutParams());
         return c;
     }
@@ -204,75 +261,98 @@ public class TeamManager extends AppCompatActivity implements AdapterView.OnItem
      * @param v - current window
      */
     public void onNewPlayer(View v){
-        TableRow r = new TableRow(this);
-        EditText n,num,pos;
+        TableRow r;
+        EditText n;
+        EditText num;
+        EditText pos;
         CheckBox chk;
-        r.addView(n =newName());
-        r.addView(num = newNumber());
-        r.addView(pos = newPos());
-        r.addView(chk = newCheck());
+        r = new TableRow(this); //create new table row
+        //create new control for table row
+        n = newName();
+        num = newNumber();
+        pos = newPos();
+        chk = newCheck();
+        //add controls to table row
+        r.addView(n);
+        r.addView(num);
+        r.addView(pos);
+        r.addView(chk);
+        //add table row to table
         tbl_players.addView(r,tbl_row.getLayoutParams());
+        //update player count on screen
         playersCount++;
         txt_numPlayers.setText(Integer.toString(playersCount));
     }
 
-    // TODO Add way to scroll player list so that the cell being edited is above the popup keyboard
 
     /**
      * method to save changes to team when the save changes button is pressed
      * @param v - current window
      */
     public void onSaveChanges(View v){
-        Player p;
-        Team t;
-        String numString;
-        int num;
+        Player p; //player
+        Team t; //team player is on
+        String numString; //player number as a string
+        int num; //player number
+        EditText name; //name control
+        EditText number; //number control
+        EditText position; //position control
+        CheckBox active; //active checkbox control
+        TableRow r; //row in table
+
 
         t = selectedTeam;
+        //if there is not enough players in the table
         if(tbl_players.getChildCount() <= 1){
-            //error, no players.  Remove the Team
+            //display error message, no players and remove the team
             Toast.makeText(getApplicationContext(),"Error 0 players.  Team has been deleted",Toast.LENGTH_LONG).show();
             StatsManager.removeTeam(team);
             return;
         }
-        //stars as 1 because 0 is the heading
+
+        //starts as 1 because 0 is the heading
         for(int i = 1; i < tbl_players.getChildCount(); i++){
-            TableRow r = (TableRow)tbl_players.getChildAt(i);
-
-            EditText name = (EditText)r.getVirtualChildAt(0);
-            EditText number = (EditText)r.getVirtualChildAt(1);
-            EditText position = (EditText)r.getVirtualChildAt(2);
-            CheckBox active = (CheckBox)r.getVirtualChildAt(3);
-
+            //get current row
+            r = (TableRow)tbl_players.getChildAt(i);
+            //read player information
+            name = (EditText)r.getVirtualChildAt(0);
+            number = (EditText)r.getVirtualChildAt(1);
+            position = (EditText)r.getVirtualChildAt(2);
+            active = (CheckBox)r.getVirtualChildAt(3);
+            //requires name
             if(name.getText().toString().equals("")) {
                 name.setError("Must have a valid name");
                 return;
             }
+            //requires number
             if(number.getText().toString().equals("")) {
                 name.setError("Must have a valid number");
                 return;
             }
 
-
+            //get player number and converts it
             numString = number.getText().toString();
-
             try{
                 num = Integer.parseInt(numString);
             }catch(Exception x){
                 number.setError("Must have a valid number");
                 return;
             }
+            //see if player exists
             p = t.findPlayer(name.getText().toString(), num);
-
+            //if the player exists
             if(p != null){
+                //update existing player
                 p.setPosition(position.getText().toString());
                 p.setActive(active.isChecked());
-            }else{
+            }else{ //player doesn't exist
+                //create new player
                 p = new Player(name.getText().toString(), num);
                 p.setPosition(position.getText().toString());
                 p.setActive(active.isChecked());
                 t.addPlayer(p);
             }
+            //saves everything including teams
             StatsManager.toFile();
         }
     }
