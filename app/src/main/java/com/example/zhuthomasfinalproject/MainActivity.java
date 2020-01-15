831cfc5b85f3cd8621e9cc18ce5ce9d5b68eaf2d
sydney <snthomas@hotmail.ca>
2020-01-15 01:43:40 -0500

    Commented GameTimeTrackerActivity and MainActivity class

--- app/src/main/java/com/example/zhuthomasfinalproject/MainActivity.java
+++ app/src/main/java/com/example/zhuthomasfinalproject/MainActivity.java
@@ -5,21 +5,21 @@ Window that displays the options in the main menu, allowing the user to access e
 Each button opens a new window (each has its own activity class), this window is mainly used for navigation.
  */
 package com.example.zhuthomasfinalproject;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     static String path;
 
-
+    /**
+     * method that is called when the window is created
+     * @param savedInstanceState - system parameter
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
+            // We don't have permission so prompt the user to give the app permission to use files
             ActivityCompat.requestPermissions(
                     this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -37,32 +37,53 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+        //set the directory for StatsManager to use for app data files
         path = getFilesDir().getPath();
         StatsManager.setDirectory(path);
         StatsManager.initStatsManager();
 
     }
 
-
+    /**
+     * method that launches GameSetup window
+     * @param v - current window
+     */
     public void launchGameSetup(View v) {
         Intent i = new Intent(this, GameSetup.class);
         startActivity(i);
     }
 
+    /**
+     * method that launches StatModeSelection window
+     * @param v - current window
+     */
     public void launchStatModeSelection(View v) {
         Intent i = new Intent(this, StatModeSelection.class);
         startActivity(i);
     }
 
+    /**
+     * method that launches TeamManager window for creating and editing teams
+     * @param v - current window
+     */
     public void launchTeamManager(View v) {
         Intent i = new Intent(this, TeamManager.class);
         startActivity(i);
     }
 
+    /**
+     * method that launches Instructions window
+     * @param v - current window
+     */
     public void launchInstructions(View v) {
         Intent i = new Intent(this, Instructions.class);
         startActivity(i);
     }
+
+    /**
+     * method that launches Credits window
+     * @param v - current window
+     */
     public void launchCredits(View v) {
         Intent i = new Intent(this, Credits.class);
         startActivity(i);
