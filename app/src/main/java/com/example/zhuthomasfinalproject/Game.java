d2339944ca9534bcf0c042766d1ace5715f49f10
sydney <snthomas@hotmail.ca>
2020-01-15 02:01:02 -0500

    Commented Game class

--- app/src/main/java/com/example/zhuthomasfinalproject/Game.java
+++ app/src/main/java/com/example/zhuthomasfinalproject/Game.java
@@ -1,7 +1,10 @@
+/**
+ * Sydney Thomas
+ * January 15, 2020
+ * Class that encapsulates Game
+ */
 package com.example.zhuthomasfinalproject;
-
 import android.os.Environment;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,13 +15,13 @@ import java.util.Date;
 public class Game implements Serializable {
     private Team team; //team stats are being tracked for
     private String opponent; //opponent team is playing
-    private long dateTime;
+    private long dateTime; //timestamp of when the game was
     private int teamFouls; //number of team fouls
     private int points; //points team scores
     private Player Playing[]; //array that stores the 5 players currently on the floor
 
     /**
-     * Default constructor that constructs a Game class and initializes attributes
+     * Default constructor that constructs a Game object and initializes attributes
      */
     public Game(){
         opponent = "";
@@ -27,7 +30,7 @@ public class Game implements Serializable {
     }
 
     /**
-     * Constructor that constructs a Game class with parameters and initializes attributes
+     * Constructor that constructs a Game object with parameters and initializes attributes
      * @param t - team stats are being tracked for
      * @param opp - opponent team is playing
      */
@@ -37,50 +40,66 @@ public class Game implements Serializable {
         opponent = opp;
     }
 
-
+    /**
+     * accessor that gets the array of players playing in game
+     * @return - array of players playing in game
+     */
     public Player[] getPlaying() {
         return Playing;
     }
 
+    /**
+     * mutator that sets array of players playing in game
+     * @param p - array of players playing in game to set
+     */
     public void setPlaying(Player[] p) {
         Playing = p;
     }
 
     /**
-     * accessor that gets the team tracking stats for
-     * @return - team
+     * accessor that gets the team object for the team stats are being tracked for
+     * @return - team object
      */
     public Team getTeam() {
         return team;
     }
 
     /**
-     * mutator that sets the team that the stats are being tracked for
-     * @param t - team to set
+     * mutator that sets the team object that the stats are being tracked for
+     * @param t - team object to set
      */
     public void setTeam(Team t) {
         team = t;
     }
 
     /**
-     * accessor that gets the opponent team is playing
-     * @return - opponent
+     * accessor that gets the opponent that the team is playing
+     * @return - opponent name
      */
     public String getOpponent() {
         return opponent;
     }
 
     /**
-     * mutator that sets the opponent team is playing
-     * @param o - opponent to set
+     * mutator that sets the opponent name that the team is playing
+     * @param o - opponent name to set
      */
     public void setOpponent(String o) {
         opponent = o;
     }
+
+    /**
+     * accessor that gets the timestamp for the game
+     * @return - timestamp for game
+     */
     public long getGameDateTime() {
         return dateTime;
     }
 
+    /**
+     * mutator that sets the timestamp for the game
+     * @param dT - timestamp to set
+     */
     public void setGameDateTime(long dT) {
         dateTime = dT;
     }
@@ -147,42 +166,48 @@ public class Game implements Serializable {
         points -= ptsToSub;
     }
 
+    /**
+     * method that increments the amount of time that all the players on the court have played
+     * @param time - time in milliseconds to add
+     */
     public void addPlayingTime(long time) {
         for(int i = 0; i < this.Playing.length; i++) {
             Playing[i].getCurrentStats().addPlayingTime(time);
         }
     }
 
-    /**
-     * Method that creates String representation of class
-     * @return - string representation
-     */
-    public String toString(){
-        String str = "";
-        Date d = new  Date(this.dateTime);
-        Time t = new Time(this.dateTime);
-        str += "com.example.zhuthomasfinalproject.Team Name: " + team + "\nOpponent: " + opponent + "\nDate/Time " + d.toString() + " " + t.toString();
-        //TODO add missing fields
-        return str;
-    }
 
+    /**
+     * method that exports game and player stats for game to a CSV file
+     */
     public void exportToCSV() {
-        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        String date = StatsManager.getMonthFromTimestamp(dateTime) +
+        File dir;
+        String date;
+        File file;
+        String fileName;
+
+        //get downloads directory to store exported file
+        dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        //calculate date to put in CSV file name
+        date = StatsManager.getMonthFromTimestamp(dateTime) +
                 StatsManager.getDayFromTimestamp(dateTime) +
                 StatsManager.getYearFromTimestamp(dateTime);
-        File file = new File(dir, "Game " + date + " vs " + opponent);
+        //create a file object named according to the date of the game
+        fileName = dir + "Game " + date + " vs " + opponent;
+        file = new File(dir, "Game " + date + " vs " + opponent);
         System.out.println( "Export Game to " + dir.getAbsolutePath() + "/" +
                 "Game " + date + " vs " + opponent);
-        try (FileWriter fileWriter = new FileWriter(file)) {
-            // fileWriter.append("Writing to file!");
+        //file writer that goes through and writes game info followed by headings and player stats for each player
+        try(FileWriter fileWriter = new FileWriter(file)) {
+            //writes game stats
             fileWriter.write( team.getName() + " vs " + opponent + "," +
                     StatsManager.getDateTimeFromTimestamp(this.dateTime) +"\n");
-
+            //writes headings for player stats
             fileWriter.write( "Points:," + points + ",Team Fouls:,"+ teamFouls + "\n");
             fileWriter.write("Name,Number,Position,Assists, Blocks, DefReb, Fouls, FtMakes," +
                     "FtMisses,Minutes,OffReb,PlayTime,Points,Steals,3PtMake, 3PtMiss, 2PtMake, " +
                     "2PtMiss,TtlReb,Turnover\n");
+            //gets Player object for each player on the team and outputs PlayerStats for this game
             for(Player p: team.getPlayers()) {
                 PlayerStats s = p.getStat(this.dateTime);
                 if(s==null) {
@@ -198,9 +223,24 @@ public class Game implements Serializable {
                         s.getTurnovers() + "\n" );
 
             }
-
-        } catch (IOException e) {
-            //Handle exception
+        }catch (IOException e){
+            //ignore exception but output error information
+            System.err.println("Error " + e.getMessage()+" writing export file: " + fileName);
         }
     }
+
+
+    /**
+     * method that creates String representation of class
+     * @return - string representation
+     */
+    public String toString(){
+        String str = "";
+        Date d = new  Date(dateTime);
+        Time t = new Time(dateTime);
+        str += "com.example.zhuthomasfinalproject.Team Name: " + team + "\nOpponent: " + opponent +
+                "\nDate/Time " + d.toString() + " " + t.toString();
+        return str;
+    }
+
 }
